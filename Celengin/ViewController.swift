c3134e2aa94afb10c28c67222077ad2e97c52644
Kathleen Febiola <kathy.febiola02@gmail.com>
2022-03-29 19:20:48 +0700

    Initial Commit

---
+++ Celengin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Celengin
+//
+//  Created by Kathleen Febiola Susanto on 29/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
