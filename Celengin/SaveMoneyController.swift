724d3350b36fccd821aeb0649055b5a15eed0315
Kathleen <kathy.febiola02@gmail.com>
2022-04-07 19:54:06 +0700

    Add new controllers

---
+++ Celengin/SaveMoneyController.swift
@@ -0,0 +1,29 @@
+//
+//  SaveMoneyController.swift
+//  Celengin
+//
+//  Created by Kathleen Febiola Susanto on 07/04/22.
+//
+
+import UIKit
+
+class SaveMoneyController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
