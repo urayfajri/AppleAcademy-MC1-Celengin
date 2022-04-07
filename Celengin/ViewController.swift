131a739c03d7d4e5298c2ab4964660a777c00696
urayfajri <uraywidiansyah88@gmail.com>
2022-04-07 13:10:01 +0700

    merging

--- Celengin/ViewController.swift
+++ Celengin/ViewController.swift
@@ -20,16 +20,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         
-<<<<<<< HEAD
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(tapAdd))
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: nil)
-    
-      
-        
-=======
 //        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(tapAdd))
 //        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: nil)
->>>>>>> uray_form_add
+
         dummyData()
         getAllItems()
         goalTable.rowHeight = UITableView.automaticDimension
