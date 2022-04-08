ad08708c4017b958f81ff78b9ce0ed24bc319b9f
urayfajri <uraywidiansyah88@gmail.com>
2022-04-08 16:15:21 +0700

    menanmbahkan validasi goal name dan goal target

--- Celengin/FormAddViewController.swift
+++ Celengin/FormAddViewController.swift
@@ -10,21 +10,107 @@ import UIKit
 class FormAddViewController: UIViewController {
     
 
+    @IBOutlet weak var goalNameTextField: UITextField!
+    @IBOutlet weak var goalTargetTextField: UITextField!
+    
+    @IBOutlet weak var validateGoalNameTextField: UILabel!
+    @IBOutlet weak var validateGoalTargetTextField: UILabel!
+    
+    @IBOutlet weak var submitGoalButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        resetForm()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func resetForm() {
+        submitGoalButton.isEnabled = false
+        
+        validateGoalNameTextField.isHidden = false
+        validateGoalTargetTextField.isHidden = false
+        
+        
+        // message validation
+        validateGoalNameTextField.text = "Goal Name is Required"
+        validateGoalTargetTextField.text = "Goal Target is Required"
+        
+        // empty text field
+        goalNameTextField.text = ""
+        goalTargetTextField.text = ""
+        
     }
-    */
-
+    
+    @IBAction func goalNameChanged(_ sender: Any) {
+        
+        if let goalName = goalNameTextField.text
+        {
+            if let errorMessage = invalidGoalName(goalName)
+            {
+                validateGoalNameTextField.text = errorMessage
+                validateGoalNameTextField.isHidden = false
+            } else
+            {
+                validateGoalNameTextField.isHidden = true
+            }
+        }
+        checkValidForm()
+    }
+    
+    @IBAction func goalTargetChanged(_ sender: Any) {
+        
+        if let goalTarget = goalTargetTextField.text
+        {
+            if let errorMessage = invalidGoalTarget(goalTarget)
+            {
+                validateGoalTargetTextField.text = errorMessage
+                validateGoalTargetTextField.isHidden = false
+            } else
+            {
+                validateGoalTargetTextField.isHidden = true
+            }
+        }
+        checkValidForm()
+    }
+    
+    @IBAction func submitGoalTapped(_ sender: Any) {
+        resetForm()
+    }
+    
+    func invalidGoalTarget(_ value: String) -> String? {
+        
+        let set = CharacterSet(charactersIn: value)
+        if !CharacterSet.decimalDigits.isSuperset(of: set)
+        {
+            return "Goal Target must contain only digits"
+        }
+        if value.isEmpty
+        {
+            return "Goal Target is Required"
+        }
+        return nil
+    }
+    
+    func invalidGoalName(_ value: String) -> String? {
+        
+        if value.isEmpty
+        {
+            return "Goal Name is Required"
+        }
+        return nil
+    }
+    
+    func checkValidForm() {
+        if(validateGoalNameTextField.isHidden && validateGoalTargetTextField.isHidden)
+        {
+            submitGoalButton.isEnabled = true
+        }
+        else
+        {
+            submitGoalButton.isEnabled = false
+        }
+    }
+    
 }
