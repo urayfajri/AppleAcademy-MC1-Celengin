b654222d4dd330b2db957700c388507e997377e8
urayfajri <uraywidiansyah88@gmail.com>
2022-04-10 17:39:48 +0700

    add scroll view

---
+++ Celengin/FormAddIncomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FormAddIncomeViewController.swift
+//  Celengin
+//
+//  Created by Uray Muhamad Noor Fajri Widiansyah on 10/04/22.
+//
+
+import UIKit
+
+class FormAddIncomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
