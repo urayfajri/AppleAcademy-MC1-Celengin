c7cbdf1e5df02e7b3a7ce76633f540f8f9e4c185
urayfajri <uraywidiansyah88@gmail.com>
2022-04-11 04:36:43 +0700

    Add Form Add Expense Controller

---
+++ Celengin/FormAddExpenseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FormAddExpenseViewController.swift
+//  Celengin
+//
+//  Created by Uray Muhamad Noor Fajri Widiansyah on 11/04/22.
+//
+
+import UIKit
+
+class FormAddExpenseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
