5189afb404c99910330efbba1cac8a08afbd1c6f
Kathleen <kathy.febiola02@gmail.com>
2022-04-11 16:32:49 +0700

    Bug fixing

---
+++ Celengin/Helpers/Transaction+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Transaction+CoreDataClass.swift
+//  Celengin
+//
+//  Created by Kathleen Febiola Susanto on 11/04/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Transaction)
+public class Transaction: NSManagedObject {
+
+}
